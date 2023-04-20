ca794c53f0798e3e2f06048c24fa4996f4257c18
Ravermeister <bgygblbketbl@mail.ru>
2023-04-20 15:55:59 +0300

    HW 5.1

--- servlets/src/main/java/ru/netology/controller/PostController.java
+++ servlets/src/main/java/ru/netology/controller/PostController.java
@@ -1,42 +1,36 @@
 package ru.netology.controller;
 
-import com.google.gson.Gson;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.Reader;
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
-  public static final String APPLICATION_JSON = "application/json";
-  private final PostService service;
+    private final PostService service;
 
-  public PostController(PostService service) {
-    this.service = service;
-  }
+    public PostController(PostService service) {
+        this.service = service;
+    }
 
-  public void all(HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var data = service.all();
-    final var gson = new Gson();
-    response.getWriter().print(gson.toJson(data));
-  }
-
-  public void getById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
-  }
-
-  public void save(Reader body, HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var gson = new Gson();
-    final var post = gson.fromJson(body, Post.class);
-    final var data = service.save(post);
-    response.getWriter().print(gson.toJson(data));
-  }
-
-  public void removeById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
-  }
+    @GetMapping
+    public List<Post> all(HttpServletResponse response) throws IOException {
+        return service.all();
+    }
+    @GetMapping("/{id}")
+    public Post getById(@PathVariable long id, HttpServletResponse response) {
+        return service.getById(id);
+    }
+    @PostMapping
+    public Post save(@RequestBody Post post) throws IOException {
+        return service.save(post);
+    }
+    @DeleteMapping
+    public void removeById(@PathVariable long id, HttpServletResponse response) {
+        service.removeById(id);
+    }
 }
